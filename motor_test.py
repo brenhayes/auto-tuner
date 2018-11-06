658dd239347b4484d32ad810c3ddbdbfdbe0ece2
orolo <supra_1018@hotmail.com>
2018-11-06 16:57:41 -0500

    Update motor_test.py

--- motor_test.py
+++ motor_test.py
@@ -1,15 +1,20 @@
 from nanpy import ArduinoApi, SerialManager, Stepper
 from time import sleep
+print("hello")
+try:
+    connection = SerialManager()
+    a = ArduinoApi(connection=connection)
+except:
+    print("connection failed")
 
-connection = SerialManager()
-a = ArduinoApi(connection=connection)
-
+print("hello two")
 # Number of steps per revolution
 stepsPerRevolution = 1500
 # Initialize Stepper library on pins 8 through 11
-myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11)
+print(stepsPerRevolution, "Steps per rev")
+myStepper = Stepper(revsteps=stepsPerRevolution, pin1=8, pin2=10, pin3=9, pin4=11)
 myStepper.setSpeed(20)
-
+print("breakpoint")
 while True:
     print("clockwise")
     myStepper.step(stepsPerRevolution)
@@ -18,3 +23,4 @@ while True:
     print("counter clockwise")
     myStepper.step(-stepsPerRevolution)
     sleep(1000)
+
